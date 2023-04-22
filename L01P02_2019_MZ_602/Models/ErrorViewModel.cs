55cc6fc6516765ee47b415d36c7ccc927dd8a641
Luis Moreno <luis.moreno1@catolica.edu.sv>
2023-04-22 08:47:55 -0600

    Agregar archivos de proyecto.

---
+++ L01P02_2019_MZ_602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2019_MZ_602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
